b17aed55ccfc5af0b7a355b18dc500d0ade52151
William <cabiam@criptext.com>
2022-08-15 22:07:27 +0200

    updating package.json

--- index.js
+++ index.js
@@ -49,3 +49,36 @@ module.exports = User;
 
 // after configuring page section in the settings of the project
 // and there will be the link https://cabiamdos.github.io/my-package-repo/
+
+
+// ---
+// to create a new branch use 'git branch nameOfBranch'
+// 'npm i ci' will install the exact version of the packages of the 'package-lock.json' of a project
+
+// in general when a package has not a explicit license most organizations avoid to use it. to prevent potential legal risks
+// there are more than 200 open source license
+// GNU (GPL)
+// The Apachie License
+// Berkeley Software Distribution (BSD)
+// MIT License
+
+// +how to choose?
+// read contracts
+// verify your dependencies
+// choosealicense.com
+
+// when you create a 'new file', 'add file', and type LICENSE.md as the name, github will recognize it as a license and will give you an option to choose from different licenses.
+
+// REMOVING A PACKAGE
+// if we want to publish
+// npm publish
+
+// if we want to unpublish
+// npm unpublish
+// but it won't work on a package uploaded with github
+// so we need to go o the github page where the package is seleted>settings>manage-versions>delete all the versions you want to rmeove
+// and remove it
+// to remove the whole package you need to go to options and click on remove
+
+// to restore you need to go to settings>package and you will find the package you just removed
+// just click on 'restore' and confirm the name
